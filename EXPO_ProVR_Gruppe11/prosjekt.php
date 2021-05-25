36c424c3394cc28a330eadf40251fc0339ef06d0
olehjelle <69794502+olehjelle@users.noreply.github.com>
2021-05-25 16:25:39 +0200

    Removed uncessisary images, updated project.php

--- EXPO_ProVR_Gruppe11/prosjekt.php
+++ EXPO_ProVR_Gruppe11/prosjekt.php
@@ -9,25 +9,20 @@
 		<h1><a href="prosjekt.php">ProVR - D11-2021</a></h1>
 
 		<p>
-			ProVR is a virtual reality  
+			ProVR is a virtual reality application
 			<br><br>
-			Dere skal ha minst disse fire sidene (prosjekt.php, oppgaven.php, gruppen.php og linker.php). Vi ønsker at dette skal være en konkurranse om det dere har gjort og ikke en konkurranse i webdesign, derfor er malen veldig enkel - og skal følges! Deltakelse i USNExpo er obligatorisk og dette er "corona-versjonen" vår.
-			<br><br>
-			Dere skal legge dette inn på webområdet dere har fått fra skolen senest onsdag 26. mai klokken 12.00 og sende Karoline en epost så hun får registrert det og sendt det videre. Klokken 12.00 åpner vi for at juryen få se på det.
-			<br><br>
-			Vi håper vi får til en poll som dere kan dele med bestemor og mamma så de kan være med å stemme på publikumsprisen!
-			Under her skal dere ha de tre linkene, og dere skal legge inn plakaten som presenterer prosjektet deres.
+			
 		</p>
 
 		<br><br>
 		
-		<a href="gruppen.php">Les mer om gruppen vår</a><br>
-		<a href="oppgaven.php">Les mer om oppgaven</a><br>
-		<a href="linker.php">Dokumenter og linker</a><br>
+		<a href="gruppen.php">About our group</a><br>
+		<a href="oppgaven.php">About our project</a><br>
+		<a href="linker.php">External links</a><br>
 
 	</div>
 
-	<img id="Plakat" src="Bilder/Plakat.jpg" alt="Plakat">
+	<img id="Plakat" src="Bilder/Plakat_ProVR_Gruppe11.jpg" alt="Plakat">
 
 </body>
 </html>
