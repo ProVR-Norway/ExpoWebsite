2f38cac6139c6aac7df598cb2d6b294d4ffcba49
David Dokka <david_dokka92@live.no>
2021-05-25 16:22:12 +0200

    Added the expo folder example

---
+++ EXPO_ProVR_Gruppe11/EXPO_Karoline_Example/php/Heading.php
@@ -0,0 +1,15 @@
+<head>
+  <meta charset="utf-8">
+  <title>Digitalt Expo 2021</title>
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&amp;display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
+  <link rel="preload" href="/fonts/TiemposHeadlineWeb-Medium.woff" as="font" type="font/woff" crossorigin="anonymous"/>
+  <link rel="preload" href="/fonts/bootstrap/glyphicons-halflings-regular.woff2" as="font" type="font/woff" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="php/mystyle.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <div class="banner">
+     <img src="Bilder/USNexpo-banner.jpg">
+  </div>
+</head>
---
+++ EXPO_ProVR_Gruppe11/gruppen.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<?php
+    include 'php/Heading.php';
+?>
+<body>
+  <div>
+
+    <h1><a href="prosjekt.php">Tittel på prosjektet - Dxx-2021</a></h1>
+
+    <h2>Gruppen vår</h2>
+
+    <table>
+
+      <tr>
+        <td class="PersonBilde"><img src="Bilder/Karoline.jpg" alt="Karoline"></td>
+        <td>
+          Lag en rad for hver person, Fortell om bakgrunn, studieretning og ansvarsområde i prosjektet
+          <!-- <br> brukes for ny linje, kan være nyttig for å lage mellomrom mellom elementer i teksten, 
+            ellers blir det ny linje når teksten når kanten til kolonnen, 
+            øker bare antall utfra mengden mellomrom mellom linjene  -->
+          <br><br>
+          Mer tekst.
+
+        </td>
+      </tr>
+      <tr>
+        <td class="PersonBilde"><img src="Bilder/Karoline.jpg" alt="Karoline"></td>
+        <td>Dere kan godt lage en side for hver og linke til den i denne tabellen</td>
+      </tr>
+    </table>
+
+    <h2>Oppdragsgiver</h2>
+    <p>
+      Presenter oppdragsgiveren deres (dere kan godt linke til en ny side)
+    </p>
+  </div>
+</body>
+</html>
