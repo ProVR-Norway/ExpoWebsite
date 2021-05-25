cb7af5d47e25742d32b1662daba4c22206c70ece
olehjelle <69794502+olehjelle@users.noreply.github.com>
2021-05-25 16:35:59 +0200

    Updated gruppen.php

--- EXPO_ProVR_Gruppe11/gruppen.php
+++ EXPO_ProVR_Gruppe11/gruppen.php
@@ -6,7 +6,7 @@
 <body>
   <div>
 
-    <h1><a href="prosjekt.php">Tittel på prosjektet - Dxx-2021</a></h1>
+    <h1><a href="prosjekt.php">ProVR - D11-2021</a></h1>
 
     <h2>Gruppen vår</h2>
 
@@ -15,28 +15,47 @@
       <tr>
         <td class="PersonBilde"><img src="Bilder/øystein.jpg" alt="Øystein"></td>
         <td>
-          Lag en rad for hver person, Fortell om bakgrunn, studieretning og ansvarsområde i prosjektet
+          Nilsen, Øystein Løndal
           <!-- <br> brukes for ny linje, kan være nyttig for å lage mellomrom mellom elementer i teksten, 
             ellers blir det ny linje når teksten når kanten til kolonnen, 
             øker bare antall utfra mengden mellomrom mellom linjene  -->
           <br><br>
-          Mer tekst.
+          Software Engineer (Embedded Systems)
         </td>
+        Responsebilities:
+        
       </tr>
-      
+
       <tr>
         <td class="PersonBilde"><img src="Bilder/david.jpg" alt="David"></td>
-        <td>Dere kan godt lage en side for hver og linke til den i denne tabellen</td>
+        <td>Dokka, David</td>
+        <br><br>
+        Software Engineer (Virtual Systems)
+        <br><br>
+        Responsebilities:
+        
       </tr>
 
       <tr>
         <td class="PersonBilde"><img src="Bilder/ole.jpg" alt="Ole Håkon"></td>
-        <td>Dere kan godt lage en side for hver og linke til den i denne tabellen</td>
+        <td>Hjelle, Ole Håkon</td>
+        <br><br>
+        Software Engineer (Embedded Systems)
+        <br><br>
+        Responsebilities:
+        Risk Manager
+        Scrum Master
+        CAD Model Interaction
       </tr>
 
       <tr>
         <td class="PersonBilde"><img src="Bilder/david.jpg" alt="Ingvar"></td>
-        <td>Dere kan godt lage en side for hver og linke til den i denne tabellen</td>
+        <td>Wedervang, Ingvar August</td>
+        <br><br>
+        Software Engineer (Embedded Systems)
+        <br><br>
+        Responsebilities:
+        
       </tr>
     </table>
 
